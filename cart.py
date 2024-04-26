beae61b021220831f7a409d8977a78d33b85110a
Tnguyen0525 <71985660+Tnguyen0525@users.noreply.github.com>
2024-04-26 10:01:04 -0500

    contructor for cart class
    creates the contructor for the cart class

--- cart.py
+++ cart.py
@@ -1,4 +1,9 @@
-class Cart:
+import sqlite3
 
-    def __init__(self):
-        sample = ""
+class Cart:
+    # contructor for cart class
+    def __init__(self, database_Name="methods.db"):
+        self.database_Name = database_Name
+        self.connection = sqlite3.connect(database_Name)
+        self.cursor = self.connection.cursor()
+   
