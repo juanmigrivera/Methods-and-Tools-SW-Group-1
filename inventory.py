47965cd79b062cdff349a3ccd4b2933550328f35
juanmigrivera <166737886+juanmigrivera@users.noreply.github.com>
2024-04-26 02:48:51 -0500

    Update inventory.py
    I finished the viewInventory and the search_inventory function. I have the getters, setters, and decrease  inventory functions to do. Although it is not included in the design document, I have thoughts to add a increase inventory function as well.

--- inventory.py
+++ inventory.py
@@ -1,10 +1,36 @@
+import sqlite3
+
 class Inventory:
 
-    def __init__(self, databaseName="methods.db"):
-        self.databaseName = databaseName
+    def __init__(self, database_Name="methods.db"):
+        self.database_Name = database_Name
+        self.connection = sqlite3.connect(database_Name)
+        self.cursor =self.connection.cursor()
 
     def viewInventory(self):
+        self.cursor.execute("SELECT * FROM Inventory")
+        inventory = self.cursor.fetchall()
+        if inventory:
+            print("Inventory:")
+            separator = "-" * 120
+            print(separator)
+            print(f"{'ISBN':<20}{'Title':<25}{'Author':<25}{'Genre':<25}{'Pages':<10}{'Release Date':<15}{'Price':<10}{'Stock':<10}")
+            print(separator)
+            for item in inventory:
+                print(f"{item[0]:<20}{item[1]:<25}{item[2]:<25}{item[3]:<25}{item[4]:<10}{item[5]:<15}{item[6]:<10}{item[7]:<10}")
+                print(separator)
+        else:
+            print("Inventory is empty.")
         
-    def searchInventory(self, title):
+    def searchInventory(self):
+        title = input ("Enter the title you want to search for: ")
+        self.cursor.execute("SELECT * FROM Inventory WHERE title=?", (title,))
+        results=self.cursor.fetchall()
+        if results:
+            print("Search Results: ")
+            for items in results:
+                print(item)
+            else
+                print("No results found for '{}'.".format(title))
 
     
