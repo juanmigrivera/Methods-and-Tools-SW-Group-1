d4e53c8f5a3f7950843856969d5a6002a1eb4dc8
charlieguest5 <166868239+charlieguest5@users.noreply.github.com>
2024-04-29 01:33:01 -0500

    viewHistory and __init__ progress

--- history.py
+++ history.py
@@ -1,4 +1,23 @@
-class OrderHistory:
+import sqlite3
+import random
 
-    def __init__(self):
-        sample = ""
+class OrderHistory:
+    def __init__(self, databaseName="methods.db"):
+        self.databaseName = databaseName
+        self.connection = sqlite3.connect(databaseName)
+        self.cursor = self.connection.cursor()
+    def viewHistory(self, userID):
+        try:
+            query = """
+            SELECT OrderNumber, ItemNumber, Cost, Date
+            FROM Orders
+            WHERE UserID = ?
+            """
+            self.cursor.execute(query, (userID,))
+            orders = self.cursor.fetchall()
+            if orders:
+                print("Order History:")
+                for order in orders:
+                    print(f"Order Number: {order[0]}, Number of Items: {order[1]}, Cost: {order[2]}, Date: {order[3]}")
+            else
+                print("No order history")
