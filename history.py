324b55d95dde2595e2ae3389fd20f76664481468
charlieguest5 <166868239+charlieguest5@users.noreply.github.com>
2024-04-29 16:41:40 -0500

    createOrder progress

--- history.py
+++ history.py
@@ -6,6 +6,7 @@ class OrderHistory:
         self.databaseName = databaseName
         self.connection = sqlite3.connect(databaseName)
         self.cursor = self.connection.cursor()
+        
     def viewHistory(self, userID):
         try:
             query = """
@@ -21,6 +22,7 @@ class OrderHistory:
                     print(f"Order Number: {order[0]}, Number of Items: {order[1]}, Cost: {order[2]}, Date: {order[3]}")
             else
                 print("No order history")
+                
     def viewOrder(self, userID, orderID):
         try:
             query = "SELECT * FROM Orders WHERE UserID = ? AND OrderNumber = ?"
@@ -43,7 +45,25 @@ class OrderHistory:
                     print(f"No items for order ID")
             else:
                 print(f"Order ID does not belong to user")
+                
     def createOrder(self, userID, quantity, cost, date):
+        try:
+            while True:
+                order_number = str(random.randint(100000, 999999))
+                query = "SELECT * FROM Orders WHERE OrderNumber = ?"
+                self.cursor.execute(query, (order_number,))
+                if not self.cursor.fetchone():
+                    break
+            insert_query = "INSERT INTO Orders (OrderNumber, UserID, ItemNumber, Cost, Date) VALUES (?, ?, ?, ?, ?)"
+            self.cursor.execute(insert_query, (order_number, userID, quantity, cost, date))
+            self.connection.commit()
+            return order_number
+
+    def addOrderItems(self, userID, orderID):
+        try:
+            
+    
+                
         
                 
             
